9a5c0c05724b6fdc67a0727ce8d7fa714d6c9c5c
Johnson Hsu <specialline4107@gmail.com>
2022-09-22 22:26:02 +0800

    Update read file and extend the purchase record

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+# read file
 products = []
+with open('products.csv', 'r') as f:
+	for line in f:
+		if 'Product,Price' in line:
+			continue # directly jump to next loop, meaning skip line 7 & 8 
+		name, price = line.strip().split(',') # strip \n, and split string into string list [name, price]
+		products.append([name, price])
+print(products)
+
+# User input
 while True:
 	name = input('Please input the product name: ')
 	if name == 'q': # Quit
@@ -9,12 +19,13 @@ while True:
 	products.append([name, price])
 print('The product and price list is shown as:', products)
 
+# Print all products and prices
 for p in products:
 	print(p)
 	print('The price of', p[0], 'is', p[1])
 
+# write user input into csv
 with open('products.csv', 'w') as f:
-	f.write('This is my purchase record' + ' \n')
 	f.write('Product,Price\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
