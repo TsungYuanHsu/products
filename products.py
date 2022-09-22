f94758ebf2ed7979e41e91c13143d5df2a3eb1f1
Johnson Hsu <specialline4107@gmail.com>
2022-09-22 16:13:16 +0800

    Add file

--- products.py
+++ products.py
@@ -12,3 +12,9 @@ print('The product and price list is shown as:', products)
 for p in products:
 	print(p)
 	print('The price of', p[0], 'is', p[1])
+
+with open('products.csv', 'w') as f:
+	f.write('This is my purchase record' + ' \n')
+	f.write('Product,Price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
